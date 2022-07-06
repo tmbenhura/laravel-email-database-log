7d7bb213acb61964bfb51df7a424ceeba28cab6d
dmcbrn <dmcbrn9@gmail.com>
2022-07-07 00:44:16 +0200

    properly call mailgun.filter_unknown_emails config value

--- src/Events/MailgunEvent.php
+++ src/Events/MailgunEvent.php
@@ -28,7 +28,7 @@ class MailgunEvent extends Event
         //get email
         $mail_id_str = strtok($request->{'event-data'}['message']['headers']['message-id'], '@');
         $email = $this->getEmail($mail_id_str);
-        if(!$email && config('email_log.email.filter_unknown_emails')) {
+        if(!$email && config('email_log.mailgun.filter_unknown_emails')) {
             return response('Error: no E-mail found', 200)->header('Content-Type', 'text/plain');
         }
 
