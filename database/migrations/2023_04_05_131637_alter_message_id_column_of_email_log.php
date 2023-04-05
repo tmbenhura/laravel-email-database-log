24bc5637c85eb40c94691891d2198cd2afe0e87a
tmbenhura <tmbenhura@gmail.com>
2023-04-05 13:24:22 +0200

    Update message id column to allow uuid

---
+++ database/migrations/2023_04_05_131637_alter_message_id_column_of_email_log.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMessageIdColumnOfEmailLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('email_log', function ($table) {
+            $table->string('messageId', 36)->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('email_log', function ($table) {
+            $table->string('messageId', 32)->nullable()->default(null)->change();
+        });
+    }
+}
